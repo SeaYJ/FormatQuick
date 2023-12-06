9f416aca50e44d2d7e8c22efe3827c7ef31bc1b2
SeaYJ <seayj21@outlook.com>
2023-12-06 14:54:20 +0800

    该文件是对 QT 中 QLineEdit 控件的重写，主要为了实现以下功能： 1. 双击输入栏可以打开 Windows 的文件夹选择窗口，选择“输出目录”； 2. 当自定义输入“输出目录”时，会自动在输入结束后检查文件夹是否被创建： 	- 如果文件夹已经存在，则认可本次输入； 	- 如果文件夹不存在，则询问是否立即创建该文件夹： 		- 如果立即创建，则立刻执行； 		- 如果不立即创建，则忽略（允许这个目录暂时不存在）。 注意：这要求在转换进行时一定要检查输出目录是否存在！！！！！！！

---
+++ xoutputdir.cpp
@@ -0,0 +1,79 @@
+﻿#include "xoutputdir.h"
+
+#include <QDebug>
+
+XOutputDir::XOutputDir(QWidget *parent)
+    :QLineEdit(parent)
+{
+    connect(this, &QLineEdit::editingFinished, this, &XOutputDir::checkFilePath);
+}
+
+XOutputDir::~XOutputDir()
+{
+
+}
+
+void XOutputDir::mouseDoubleClickEvent(QMouseEvent *e)
+{
+    if (e->button() == Qt::LeftButton)
+    {
+        QString OutputDir = QFileDialog::getExistingDirectory(
+            this,
+            QString(tr("选择输出路径")),
+            QStandardPaths::writableLocation(QStandardPaths::DocumentsLocation) + "/",
+            QFileDialog::ShowDirsOnly);
+
+        // 这里要判断文件夹是否存在
+        // 当用户取消本次选择时，可能会出现路径不存在问题
+        QFileInfo OutputDirInfo(OutputDir);
+        if (OutputDirInfo.isDir())
+            this->setText(OutputDir + "/");
+    }
+
+    // 保留默认双击事件
+    QLineEdit::mouseDoubleClickEvent(e);
+}
+
+void XOutputDir::checkFilePath()
+{
+    // 暂时禁用 editingFinished 信号
+    // 防止用户一次回车等特殊操作连续触发多次 editingFinished 信号，导致该函数被连续调用多次
+    blockSignals(true);
+
+    QString FilePath = this->text();
+    QDir Dir(FilePath);
+    QMessageBox ErrMsgBox;
+    QPushButton* CreateDirBtn = ErrMsgBox.addButton(QString(tr("立即创建")), QMessageBox::YesRole);
+    QPushButton* IgnoreBtn = ErrMsgBox.addButton(QString(tr("暂时忽略")), QMessageBox::YesRole);
+    QString ErrMsgBoxTitle = QString(tr("错误"));
+    QString ErrMsgBoxContent = QString("");
+
+    // 检查文件路径是否存在
+    if (!Dir.exists()) {
+        // 文件路径不存在，询问是否创建文件夹
+        ErrMsgBoxContent = QString(tr("文件路径：") + FilePath + tr("\t不存在！\n请问是否需要立即创建该文件夹？"));
+
+        ErrMsgBox.setIcon(QMessageBox::Warning);
+        ErrMsgBox.setWindowTitle(ErrMsgBoxTitle);
+        ErrMsgBox.setText(ErrMsgBoxContent);
+        ErrMsgBox.exec();
+
+        if (ErrMsgBox.clickedButton() == CreateDirBtn)
+        {
+            // 使用 QDir 立即创建文件路径
+            if (Dir.mkpath(FilePath)) {
+                qDebug() << "Folder created successfully using QDir.";
+            } else {
+                qDebug() << "Failed to create folder using QDir.";
+            }
+        }
+        else if (ErrMsgBox.clickedButton() == IgnoreBtn)
+        {
+            // 忽略
+        }
+    }
+
+    // 恢复信号
+    blockSignals(false);
+}
+
---
+++ xoutputdir.h
@@ -0,0 +1,31 @@
+﻿#ifndef XOUTPUTDIR_H
+#define XOUTPUTDIR_H
+// 设置 UTF-8 编码
+#pragma execution_character_set("UTF-8")
+
+#include <QObject>
+#include <QLineEdit>
+#include <QMouseEvent>
+#include <QFileDialog>
+#include <QStandardPaths>
+#include <QDir>
+#include <QMessageBox>
+#include <QPushButton>
+
+#include <QDebug>
+class XOutputDir : public QLineEdit
+{
+    Q_OBJECT
+public:
+    explicit XOutputDir(QWidget *parent = 0);
+    ~XOutputDir();
+
+protected:
+    virtual void mouseDoubleClickEvent(QMouseEvent *e);
+
+private slots:
+    void checkFilePath();
+
+};
+
+#endif // XOUTPUTDIR_H
